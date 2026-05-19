065740637c4eee7dc5067da5248053c5254d4f2d
VANSH KALRA <vanshkalra2406@gmail.com>
2026-05-20 01:54:12 +0530

    fixed api structure

--- riseroute/api/openai.js
+++ riseroute/api/openai.js
@@ -9,7 +9,7 @@ export default async function handler(req, res) {
           "Content-Type": "application/json",
         },
         body: JSON.stringify({
-          model: "meta-llama/llama-3-8b-instruct:free",
+          model: "google/gemma-2-9b-it:free",
           messages: [
             {
               role: "user",
